2267c04d3b57a14b8c9814fdef463b1249fcf1b5
Justin Bankes <12632465+jbankes@users.noreply.github.com>
2021-08-03 04:39:45 +0000

    feat: completed getting started with svelte from Auth0 and Brittney Postma

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # svelte_playground
+
 A repository to experiment with and learn Svelte
+
+## Contents
+
+* Intro with Brittney Postma - Code from following along with the [Auth0 YouTube Video - Get Started with Svelte and SvelteKit](https://youtu.be/fOD_3iSiwTQ)
---
+++ intro_with_bp/my-app/src/routes/about.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+  import { browser, dev } from "$app/env"
+  export const hydrate = dev
+  export const router = browser
+  export const prerender = true
+</script>
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+<section>
+  <h1>About</h1>
+  <p>this is the about page.</p>
+</section>
